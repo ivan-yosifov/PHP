fa2e032dc08f427a7474c6529432cd8a62743328
Ivan Yosifov <ivan_yosifov@yahoo.com>
2017-01-11 14:05:43 +0200

    Added SQL table script and PDO cheatsheet

---
+++ task-list/create-table.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once 'connect.php';
+
+$table = "CREATE TABLE IF NOT EXISTS tasks (
+          id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+          name VARCHAR(35) NOT NULL UNIQUE,
+          description VARCHAR(255) NOT NULL,
+          status VARCHAR(25) DEFAULT 'Not Completed',
+          created_at TIMESTAMP)";
+try{
+  $conn->query($table);
+  echo "<br>Table created";
+}catch(PDOException $ex){
+  echo "<br>An error occurred: " . $ex->getMessage();
+}
